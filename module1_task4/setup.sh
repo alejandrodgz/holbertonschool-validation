62c724279d374658bc532cbb886b34f558bf9eae
alejandrodgz <5199@holbertonstudents.com>
2023-01-11 17:48:06 -0500

    task0

--- module1_task4/setup.sh
+++ module1_task4/setup.sh
@@ -9,7 +9,7 @@ apt install -y git
 apt install -y hugo
 apt update
 apt-get update && apt-get install make
-apt install curl
+apt install -y curl
 curl -L https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb -o hugo.deb
 apt install ./hugo.deb
 
