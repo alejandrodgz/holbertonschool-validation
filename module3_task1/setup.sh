f43a7a03c26a63533b1a1f3092ba75eab35fc486
alejandrodgz <5199@holbertonstudents.com>
2023-01-12 09:02:48 -0500

    tasl

--- module3_task1/setup.sh
+++ module3_task1/setup.sh
@@ -9,8 +9,8 @@ sudo apt install -y git
 sudo apt install -y hugo
 sudo apt update
 sudo apt-get update && apt-get install -y make
-apt install -y wget
-wget https://github.com/gohugoio/hugo/releases/download/v0.84.0/hugo_extended_0.84.0_Linux-64bit.deb
+sudo apt install -y wget
+sudo wget https://github.com/gohugoio/hugo/releases/download/v0.84.0/hugo_extended_0.84.0_Linux-64bit.deb
 sudo dpkg -i hugo_extended_0.84.0_Linux-64bit.deb
 #* When running the command `make build`, there is a bunch of errors which end with the following lines:
 make build
