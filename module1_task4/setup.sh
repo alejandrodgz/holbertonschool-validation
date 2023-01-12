3274aa6b623d4308929b94774e24ccb991ad2039
alejandrodgz <5199@holbertonstudents.com>
2023-01-12 08:51:06 -0500

    test3

--- module1_task4/setup.sh
+++ module1_task4/setup.sh
@@ -4,14 +4,14 @@
 
 #* The production website is hosted in an Ubuntu 18.04 Docker container
 #* The applications "GoHugo" and "Make" are installed with `apt-get update && apt-get install -y hugo make`.
-apt update
-apt install -y git
-apt install -y hugo
-apt update
-apt-get update && apt-get install make
-apt install -y curl
-curl -L https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb -o hugo.deb
-apt install ./hugo.deb
+sudo apt update
+sudo apt install -y git
+sudo apt install -y hugo
+sudo apt update
+sudo apt-get update && apt-get install make
+sudo apt install -y curl
+sudo curl -L https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb -o hugo.deb
+sudo apt install ./hugo.deb
 
 #* When running the command `make build`, there is a bunch of errors which end with the following lines:
 make build
