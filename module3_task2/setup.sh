4a9f2ff4dd653f950646d825f4114526dc6b1e35
alejandrodgz <5199@holbertonstudents.com>
2023-01-12 12:44:47 -0500

    task2

--- module3_task2/setup.sh
+++ module3_task2/setup.sh
@@ -5,10 +5,7 @@
 #* The production website is hosted in an Ubuntu 18.04 Docker container
 #* The applications "GoHugo" and "Make" are installed with `apt-get update && apt-get install -y hugo make`.
 sudo apt update
-sudo apt install -y git
-sudo apt install -y hugo
-sudo apt update
-sudo apt-get update && apt-get install make
+sudo apt-get update && apt-get install -y make
 sudo apt install -y curl
 sudo curl -L https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb -o hugo.deb
 sudo apt install ./hugo.deb
