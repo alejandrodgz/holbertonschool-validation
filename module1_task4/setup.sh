0c951b95e7b271b3b963205800370df4cbfa90ba
alejandrodgz <5199@holbertonstudents.com>
2023-01-12 09:00:46 -0500

    tasl

--- module1_task4/setup.sh
+++ module1_task4/setup.sh
@@ -12,6 +12,5 @@ sudo apt-get update && apt-get install -y make
 apt install -y wget
 wget https://github.com/gohugoio/hugo/releases/download/v0.84.0/hugo_extended_0.84.0_Linux-64bit.deb
 sudo dpkg -i hugo_extended_0.84.0_Linux-64bit.deb
-
 #* When running the command `make build`, there is a bunch of errors which end with the following lines:
 make build
--- module3_task1/setup.sh
+++ module3_task1/setup.sh
@@ -4,14 +4,13 @@
 
 #* The production website is hosted in an Ubuntu 18.04 Docker container
 #* The applications "GoHugo" and "Make" are installed with `apt-get update && apt-get install -y hugo make`.
-apt update
-apt install -y git
-apt install -y hugo
-apt update
-apt-get update && apt-get install make
-apt install -y curl
-curl -L https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb -o hugo.deb
-apt install ./hugo.deb
-
+sudo apt update
+sudo apt install -y git
+sudo apt install -y hugo
+sudo apt update
+sudo apt-get update && apt-get install -y make
+apt install -y wget
+wget https://github.com/gohugoio/hugo/releases/download/v0.84.0/hugo_extended_0.84.0_Linux-64bit.deb
+sudo dpkg -i hugo_extended_0.84.0_Linux-64bit.deb
 #* When running the command `make build`, there is a bunch of errors which end with the following lines:
 make build
